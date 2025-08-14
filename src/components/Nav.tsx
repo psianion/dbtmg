8664b84dee39567ceb0065cc6c770072eff4b451
Sainayan <63551368+psianion@users.noreply.github.com>
2025-08-14 15:33:56 +0530

    Update Nav.tsx
    Update Investor link

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -73,7 +73,7 @@ const NavMobile = () => {
               Philanthropy
             </div>
             <div
-              onClick={() => handleNavigation('/investor-relations')}
+              onClick={() => window.location.href = 'https://old.dbrealty.co.in/investor-relations.php'}
               className={defaultStyle}
             >
               Investor Relations
@@ -203,7 +203,7 @@ const Nav = ({ activePage }: { activePage?: string }) => {
             </div>
             <div
               className={cn(defaultStyle)}
-              onClick={() => handleNavigation('/investor-relations')}
+              onClick={() => window.location.href = 'https://old.dbrealty.co.in/investor-relations.php'}
             >
               Investor Relations
             </div>
