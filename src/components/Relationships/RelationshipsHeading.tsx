e35b212153d7b8950a1091156991f47fd1243d9f
Rishabh Mahto <itsrishabh13@gmail.com>
2025-12-12 09:00:31 +0530

    chore: removed images from relationship page

--- src/components/Relationships/RelationshipsHeading.tsx
+++ src/components/Relationships/RelationshipsHeading.tsx
@@ -178,7 +178,7 @@ const RelationshipsHeading = () => {
             <p className="text-lg font-semibold ">{item.name}</p>
             <p className="text-sm font-light leading-5">{item.description}</p>
             <div className="flex gap-4 flex-wrap justify-center">
-              {item.name === "Professional Affiliations." &&
+              {/* {item.name === "Professional Affiliations." &&
                 item.images.map((_, index) => (
                   <div
                     style={{
@@ -186,7 +186,7 @@ const RelationshipsHeading = () => {
                     }}
                     className="w-[180px] lg:w-[200px] h-[100px] bg-center bg-contain bg-no-repeat"
                   />
-                ))}
+                ))} */}
             </div>
             {/* <div className='flex flex-wrap'>
               {[...Array(item.names)].map((_, index) => (
